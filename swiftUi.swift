a8311909e92437e509fcb4a8295a563c9c8efc8f
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-08-02 12:32:39 +0300

    Update swiftUi.swift

--- swiftUi.swift
+++ swiftUi.swift
@@ -1,1 +1,16 @@
+import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        
+        Text("Little limon restaurant")
+            .font(.title)
+                .imageScale(.large)
+                .foregroundColor(.yellow)
+                .padding()
+                .background(Color.purple)
+                .fixedSize(horizontal: false, vertical: true)
+        
+       
+    }
+}
